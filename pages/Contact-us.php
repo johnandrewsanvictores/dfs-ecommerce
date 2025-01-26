23a96e7eabd91aa09a39f763b8f4735a5d9188c9
Rhesty <adormeorhesty@gmail.com>
2025-01-26 21:50:15 +0800

    Added Contact-us page

---
+++ pages/Contact-us.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact us</title>
+</head>
+
+<body>
+    <h2> Get In Touch</h2>
+    <p>Adress</p>
+    <p>Phone </p>
+</body>
+
+</html>
