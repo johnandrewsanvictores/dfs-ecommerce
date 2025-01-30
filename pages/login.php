70d4bf7671451c8a39eae808211312b3795626aa
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2025-01-31 07:24:38 +0800

    finished login frontend

--- pages/login.php
+++ pages/login.php
@@ -17,18 +17,43 @@
     <?php require("../includes/navbar.php") ?>
 
     <div class="login-page">
-        <div class="login-image">
-            <img src="../assets/images/shopping.svg" alt="E-commerce Image">
-        </div>
-        <div class="login-form">
-            <h2>Login</h2>
-            <form action="login_process.php" method="post">
-                <input type="text" name="username" placeholder="Username" required>
-                <input type="password" name="password" placeholder="Password" required>
-                <button type="submit">Login</button>
-                <a href="forgot_password.php" class="forgot-password">Forgot Password?</a>
-                <p class="register-link">Don't have an account? <a href="register.php">Register here</a></p>
-            </form>
+        <div class="login-container">
+            <div class="login-left">
+                <div class="text-content">
+                    <h1>Welcome Back!</h1>
+                    <p>Sign in to continue your fashion journey</p>
+                </div>
+                <div class="illustration">
+                    <img src="../assets/images/main/login.svg" alt="Login Illustration">
+                </div>
+            </div>
+            <div class="login-right">
+                <div class="login-form-container">
+                    <h2>Sign In</h2>
+                    <form action="login_process.php" method="post">
+                        <div class="form-group">
+                            <label for="username">Username</label>
+                            <input type="text" id="username" name="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <div class="password-input">
+                                <input type="password" id="password" name="password">
+                                <i class="fas fa-eye-slash toggle-password"></i>
+                            </div>
+                        </div>
+                        <div class="form-options">
+                            <div class="remember-me">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">Remember me</label>
+                            </div>
+                            <a href="forgot_password.php" class="forgot-password">Forgot Password?</a>
+                        </div>
+                        <button type="submit" class="login-btn">Sign In</button>
+                    </form>
+                    <p class="register-link">Don't have an account? <a href="register.php">Register here</a></p>
+                </div>
+            </div>
         </div>
     </div>
 </body>
