f0f496f303e9bf9c9df4a4c46d466a86d6b8d245
fen12rir <andreidiomano@gmail.com>
2025-02-15 12:47:21 +0000

    result page

--- pages/home.php
+++ pages/home.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
@@ -234,19 +233,3 @@
 
 
 
-=======
-    <link rel="preconnectionect" href="https://fonts.googleapis.com">
-    <link rel="preconnectionect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Lobster&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../global.css">
-    <title>HOME</title>
-</head>
-
-<body>
-    <?php require("../includes/navbar.php") ?>
-
-</body>
-
-</html>
->>>>>>> feature/nav
